a9e9e4ab22d18a59f101756d8f2cb4c3acfcb330
Jonah Barrera <116769667+jxnah7@users.noreply.github.com>
2024-03-20 19:09:43 -0500

    Create Placeholders.py

---
+++ Placeholders.py
@@ -0,0 +1,16 @@
+#Placeholders in strings
+
+name = "jake"
+sentence = "%s is %d years old"
+print(sentence % ("Jonah", 22))
+#above prints "Jonah is 22 years old"
+
+
+name1 = "Jackson"
+print(f"Hello, {name1}")
+#above prints "Hello Jackson"
+
+x = 10
+y = 25
+print(f"The sum of x and y is {x+y}")
+#above prints "The sum of x and y is 35"
