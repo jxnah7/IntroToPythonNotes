389289d2e6b4d29f1b49997ed74392631e5eea54
Jonah Barrera <116769667+jxnah7@users.noreply.github.com>
2024-03-21 16:25:47 -0500

    Create builtInFunctions.py

---
+++ builtInFunctions.py
@@ -0,0 +1,16 @@
+# Built in python functions
+
+print(abs(-24)) # abs stands for absolute so it will always be positive
+
+print(bool(0)) # checks if true or false , 0 is false, anything else is true 
+
+print(dir('Hello')) # like a flashlight it lets you see all the possible string methods
+
+print(help('Hello'.upper)) # you can replace upper with any method you want more info on 
+
+sent = 'Yellow'
+print(help(sent.upper)) # this is the same as above just different way to perform it
+
+print('Hello' + str(100)) # type conversion makes 100 a string 
+print(123 + int('456')) # type conversion makes 456 an integer to perform addition
+print(float('123.45') + 0.23) # type conversion to make 123.45 a float and perform real addition
