a15d9319eb44271db42f10d2adc56ffc71b3925b
Jonah Barrera <116769667+jxnah7@users.noreply.github.com>
2024-03-20 22:59:54 -0500

    Create conditionalStatements.py

---
+++ conditionalStatements.py
@@ -0,0 +1,15 @@
+# Conditional Statements
+
+age = 16
+
+if age < 13:
+    print("You are a child")
+elif age >= 13 and age <= 18:
+    print("You are a teenager")
+elif age <= 0 or age >= 100:
+    print("Invalid age")
+else:
+    print("You are an adult")
+    
+# logical operators
+# and / or
