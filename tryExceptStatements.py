4b32eea05c8d2f69d5a16da56ca6986fdab1812f
Jonah Barrera <116769667+jxnah7@users.noreply.github.com>
2024-03-21 00:46:28 -0500

    Create tryExceptStatements.py

---
+++ tryExceptStatements.py
@@ -0,0 +1,18 @@
+# Try and except statements, control flow
+
+try:
+    if name < 3:
+        print("Hello")
+
+except:
+    print("An error was detected.")
+    
+    
+    
+
+
+
+
+# try and except are very similar to if else statements
+# if at any point an error occurs in the try statements
+# python will switch to the except block
